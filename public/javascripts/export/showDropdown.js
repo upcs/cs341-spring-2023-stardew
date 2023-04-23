e7a1e82f8cb466b1ed1b76593bf065afd33232b0
mnguytwofive <97265588+mnguytwofive@users.noreply.github.com>
2023-04-22 18:47:56 -0700

    Update showDropdown.js

--- public/javascripts/export/showDropdown.js
+++ public/javascripts/export/showDropdown.js
@@ -5,3 +5,11 @@ function showDropdownLevel(value) {
 function showDropdownSection(value) {
     document.getElementById("datatwo").setAttribute("value", value); 
 }
+
+function checkLevelandSection() {
+    if (document.getElementById("data").value.length + document.getElementById("datatwo").value.length > 1) {
+        console.log("true");
+        //do posting here
+    }
+}
+
