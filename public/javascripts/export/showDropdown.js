51000632535883e9efee17276c6adb485e48b54e
mnguytwofive <97265588+mnguytwofive@users.noreply.github.com>
2023-04-29 15:23:19 -0700

    Update showDropdown.js

--- public/javascripts/export/showDropdown.js
+++ public/javascripts/export/showDropdown.js
@@ -3,7 +3,27 @@ function showDropdownLevel(value) {
 }
 
 function showDropdownSection(value) {
-    document.getElementById("datatwo").setAttribute("value", value); 
+    document.getElementById("datatwo").setAttribute("value", value);
+
+    for (let i = 1; i < 9; i++) {
+
+        const btn = document.getElementById(i);
+        btn.style.display = "inline-block";
+    }
+
+    
+}
+
+function showPlantSection(value) {
+    document.getElementById("datathree").setAttribute("value", value);
+
+    for (let i = 1; i < 9; i++) {
+
+        const btn = document.getElementById(i);
+        btn.style.display = "inline-block";
+    }
+
+    
 }
 
 function checkLevelandSection() {
